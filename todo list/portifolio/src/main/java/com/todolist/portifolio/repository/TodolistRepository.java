4900473207e88ab555991814ce95b0c457b00627
Fabricio Ramos Coelho <81585341+FabricioRamosOficial@users.noreply.github.com>
2024-03-23 13:55:41 -0300

    Delete todo list/portifolio directory

--- todo list/portifolio/src/main/java/com/todolist/portifolio/repository/TodolistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todolist.portifolio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.todolist.portifolio.model.Afazeres;
-
-public interface TodolistRepository extends JpaRepository<Afazeres, Long> {
-
-}
